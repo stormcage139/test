28b4d156e7064a898585f271f69d374dea56a51c
stormcage <streamersochnik@gmail.com>
2024-07-22 15:42:10 +0300

    first try

--- test1.py
+++ test1.py
@@ -3,9 +3,14 @@ import telebot
 bot = telebot.TeleBot("7330806602:AAFrWTvYcEaFXeX8eNBQjEqXVPwGjF5kRZY")
 
 
-@bot.message_handler(commands=["start"])
+@bot.message_handler(commands=["start", "hello"])
 def start(message):
-    bot.send_message(message.chat.id, "Привет")
+    bot.send_message(message.chat.id, f"Привет, {message.from_user.username}")
+
+
+@bot.message_handler(commands=["qqq"])
+def start(message):
+    bot.send_message(message.chat.id, message)
 
 
 bot.infinity_polling()
