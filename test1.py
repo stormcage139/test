06e2cd6eb9881a04aa0ec2e81e30527f592e1f25
stormcage <streamersochnik@gmail.com>
2024-07-22 15:53:01 +0300

    first try

--- test1.py
+++ test1.py
@@ -3,9 +3,12 @@ import webbrowser
 
 bot = telebot.TeleBot("7330806602:AAFrWTvYcEaFXeX8eNBQjEqXVPwGjF5kRZY")
 
+
 @bot.message_handler(commands=["site"])
 def start(message):
     webbrowser.open("https://github.com/stormcage139/test/blob/master/test1.py")
+
+
 @bot.message_handler(commands=["start", "hello"])
 def start(message):
     bot.send_message(message.chat.id, f"Привет, {message.from_user.last_name} {message.from_user.first_name}")
