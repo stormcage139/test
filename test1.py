1f129f8a60fc525cd5c7c04c764f9e40ceadab17
stormcage <streamersochnik@gmail.com>
2024-07-22 20:04:55 +0300

    Third ver

--- test1.py
+++ test1.py
@@ -7,13 +7,17 @@ bot = telebot.TeleBot("7330806602:AAFrWTvYcEaFXeX8eNBQjEqXVPwGjF5kRZY")
 
 @bot.message_handler(commands=["start"])
 def start_button(message):
-    markup = types.ReplyKeyboardMarkup()
-    btn = types.KeyboardButton("Исходный код бота")
-    btn1 = types.KeyboardButton("Элемент гайда по которому сделаны кнопки")
-    markup.row(btn)
-    markup.row(btn1)
-    bot.send_message(message.chat.id, "Что вас интересует?", reply_markup=markup)
-    bot.register_next_step_handler(message, onclick)
+    tony_stark = open("./1.jpg", "rb")
+    markup = types.InlineKeyboardMarkup()
+    btn = types.InlineKeyboardButton("Исходный код бота",
+                                     url="https://github.com/stormcage139/test/blob/master/test1.py")
+    btn1 = types.InlineKeyboardButton("Элемент гайда по которому сделаны кнопки",
+                                      url="https://youtu.be/RpiWnPNTeww?si=cY81QoEYM6rpc1b4&t=996")
+    markup.row(btn, btn1)
+    bot.send_photo(message.chat.id, tony_stark)
+    bot.send_message(message.chat.id, "Данный бот создан для изучение библиотеки telebot,так что скудный "
+                                      "функцинал прошу не осуждать", reply_markup=markup)
+    # bot.register_next_step_handler(message, onclick)
 
 
 def onclick(message):
@@ -24,11 +28,6 @@ def onclick(message):
     bot.register_next_step_handler(message, onclick)
 
 
-@bot.message_handler(commands=["site"])
-def site(message):
-    webbrowser.open("https://github.com/stormcage139/test/blob/master/test1.py")
-
-
 @bot.message_handler(commands=["hello"])
 def start(message):
     bot.send_message(message.chat.id, f"Привет, {message.from_user.last_name} {message.from_user.first_name}")
@@ -37,17 +36,6 @@ def start(message):
 @bot.message_handler(commands=["user"])
 def qqq(message):
     bot.send_message(message.chat.id, message)
-
-
-@bot.message_handler(commands=["test"])
-def test(message):
-    markup = types.InlineKeyboardMarkup()
-    btn1 = types.InlineKeyboardButton("Вам выдан доступ на сайт с кодом",
-                                      url="https://github.com/stormcage139/test/blob/master/test1.py")
-    btn2 = types.InlineKeyboardButton("Delete", callback_data="LOL")
-    markup.row(btn1, btn2)
-    markup.row(btn2)
-    bot.send_message(message.chat.id, "Ку", reply_markup=markup)
 
 
 @bot.message_handler(commands=["auto"])
@@ -63,10 +51,10 @@ def signin(message):
         bot.reply_to(message2, "ЛЕХЕНДА", reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda callback: True)
-def callback_mess(callback):
-    if callback.data == "LOL":
-        bot.delete_message(callback.message.chat.id, callback.message.message_id - 1)
+# @bot.callback_query_handler(func=lambda callback: True)
+# def callback_mess(callback):
+#     if callback.data == "LOL":
+#         bot.delete_message(callback.message.chat.id, callback.message.message_id - 1)
 
 
 bot.infinity_polling()
