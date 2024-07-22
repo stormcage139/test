83144ff696df9c633c3a8e7e779cf92484cfe4aa
stormcage <streamersochnik@gmail.com>
2024-07-22 16:04:24 +0300

    first try

--- test1.py
+++ test1.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot("7330806602:AAFrWTvYcEaFXeX8eNBQjEqXVPwGjF5kRZY")
 
 
 @bot.message_handler(commands=["site"])
-def start(message):
+def site(message):
     webbrowser.open("https://github.com/stormcage139/test/blob/master/test1.py")
 
 
@@ -14,9 +14,18 @@ def start(message):
     bot.send_message(message.chat.id, f"Привет, {message.from_user.last_name} {message.from_user.first_name}")
 
 
-@bot.message_handler(commands=["qqq"])
-def start(message):
+@bot.message_handler(commands=["user"])
+def qqq(message):
     bot.send_message(message.chat.id, message)
+
+
+@bot.message_handler(commands=["auto"])
+def qqq(message):
+    bot.send_message(message.chat.id, "Скинь фото")
+
+    @bot.message_handler(content_types=["photo"])
+    def photocheck(message):
+        bot.reply_to(message, "Авторизирован")
 
 
 bot.infinity_polling()
