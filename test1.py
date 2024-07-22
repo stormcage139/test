79335a0210f1abb77a7027839e85be2f091a9b30
stormcage <streamersochnik@gmail.com>
2024-07-22 18:43:46 +0300

    first try

--- test1.py
+++ test1.py
@@ -5,12 +5,26 @@ import webbrowser
 bot = telebot.TeleBot("7330806602:AAFrWTvYcEaFXeX8eNBQjEqXVPwGjF5kRZY")
 
 
+@bot.message_handler(commands=["start"])
+def start_button(message):
+    markup = types.ReplyKeyboardMarkup()
+    btn = types.KeyboardButton("КУЛИТИ")
+    markup.row(btn)
+    bot.send_message(message.chat.id, "КУ", reply_markup=markup)
+    bot.register_next_step_handler(message, onclick)
+
+
+def onclick(message):
+    if message.text.lower() == "кулити":
+        bot.send_message(message.chat.id, "АГА")
+
+
 @bot.message_handler(commands=["site"])
 def site(message):
     webbrowser.open("https://github.com/stormcage139/test/blob/master/test1.py")
 
 
-@bot.message_handler(commands=["start", "hello"])
+@bot.message_handler(commands=["hello"])
 def start(message):
     bot.send_message(message.chat.id, f"Привет, {message.from_user.last_name} {message.from_user.first_name}")
 
@@ -23,10 +37,12 @@ def qqq(message):
 @bot.message_handler(commands=["test"])
 def test(message):
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("Вам выдан доступ на сайт с кодом",
-                                          url="https://github.com/stormcage139/test/blob/master/test1.py"))
-    bot.send_message(message.chat.id, "Ку",reply_markup=markup)
-
+    btn1 = types.InlineKeyboardButton("Вам выдан доступ на сайт с кодом",
+                                      url="https://github.com/stormcage139/test/blob/master/test1.py")
+    btn2 = types.InlineKeyboardButton("Delete", callback_data="LOL")
+    markup.row(btn1, btn2)
+    markup.row(btn2)
+    bot.send_message(message.chat.id, "Ку", reply_markup=markup)
 
 
 @bot.message_handler(commands=["auto"])
@@ -36,10 +52,16 @@ def signin(message):
     @bot.message_handler(content_types=["photo"])
     def photocheck(message2):
         markup = types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton("Вам выдан доступ на сайт с кодом",
-                                              url="https://github.com/stormcage139/test/blob/master/test1.py"))
+        btn1 = types.InlineKeyboardButton("Вам выдан доступ на сайт с кодом",
+                                          url="https://github.com/stormcage139/test/blob/master/test1.py")
+        markup.row(btn1)
         bot.reply_to(message2, "ЛЕХЕНДА", reply_markup=markup)
 
 
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_mess(callback):
+    if callback.data == "LOL":
+        bot.delete_message(callback.message.chat.id, callback.message.message_id - 1)
+
+
 bot.infinity_polling()
-# wtf
