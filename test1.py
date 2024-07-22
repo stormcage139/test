7dc0a180a5e2778471e20e91f0b0ce4b22f83e14
stormcage <streamersochnik@gmail.com>
2024-07-22 15:43:58 +0300

    first try

--- test1.py
+++ test1.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot("7330806602:AAFrWTvYcEaFXeX8eNBQjEqXVPwGjF5kRZY")
 
 @bot.message_handler(commands=["start", "hello"])
 def start(message):
-    bot.send_message(message.chat.id, f"Привет, {message.from_user.username}")
+    bot.send_message(message.chat.id, f"Привет, {message.from_user.last_name} {message.from_user.first_name}")
 
 
 @bot.message_handler(commands=["qqq"])
