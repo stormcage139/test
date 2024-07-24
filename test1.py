ae02bfe5ab484710b92148889baf188f4a25e7e1
stormcage <streamersochnik@gmail.com>
2024-07-24 23:18:40 +0300

    first try

--- test1.py
+++ test1.py
@@ -5,6 +5,10 @@ import sqlite3
 
 bot = telebot.TeleBot("7330806602:AAFrWTvYcEaFXeX8eNBQjEqXVPwGjF5kRZY")
 
+authorized = False
+admin_mode = False
+admin_secure = ["admin", "123"]
+
 
 @bot.message_handler(commands=["start"])
 def start_button(message):
@@ -26,8 +30,10 @@ def start_button(message):
                                       url="https://youtu.be/RpiWnPNTeww?si=cY81QoEYM6rpc1b4&t=996")
     btn2 = types.KeyboardButton("/Регистрация")
     btn3 = types.KeyboardButton("/Авторизация")
+    btn4 = types.KeyboardButton("/AdminPanel")
     markup.row(btn, btn1)
     markup2.row(btn2, btn3)
+    markup2.row(btn4)
     bot.send_photo(message.chat.id, tony_stark, reply_markup=markup)
     bot.send_message(message.chat.id, "Данный бот создан для изучение библиотеки telebot,так что скудный "
                                       "функцинал прошу не осуждать", reply_markup=markup2)
@@ -72,13 +78,13 @@ def signin(message):
 
 
 def signin_step2(message):
-    user_say = message.text
+    user_say = message.text.strip()
     bot.send_message(message.chat.id, "Введите пароль от аккаунта")
     bot.register_next_step_handler(message, signin_step3, user_say)
 
 
 def signin_step3(message, user_say1):
-    user_say = message.text
+    user_say = message.text.strip()
     conn = sqlite3.connect("storm_bot.sql")
     cur = conn.cursor()
 
@@ -88,7 +94,8 @@ def signin_step3(message, user_say1):
     for el in users:
         if el[2] == user_say and el[1] == user_say1:
             bot.send_message(message.chat.id, "Вы авторизованы")
-            break
+            global authorized
+            authorized = True
     cur.close()
     conn.close()
 
@@ -110,6 +117,30 @@ def users_list(call):
         bot.send_message(call.message.chat.id, info)
 
 
+@bot.message_handler(commands=["AdminPanel"])
+def admin_author_step1(message):
+    bot.send_message(message.chat.id, "Введите логин")
+    bot.register_next_step_handler(message, admin_author_step2)
+
+
+def admin_author_step2(*args):
+    message = args[0]
+    admin_name = message.text.strip()
+    bot.send_message(message.chat.id, "Введите Пароль")
+    bot.send_message(message.chat.id, message.text)
+    bot.register_next_step_handler(message, admin_author_step2, admin_name)
+
+
+def admin_author_last(*info):
+    message = info[0]
+    admin_name = info[1]
+    global admin_secure
+    global admin_mode
+    if message.text == admin_secure[1] and admin_name == admin_secure[2]:
+        bot.send_message(message.chat.id, "Вы в режиме администратора")
+        admin_mode = True
+
+
 # def onclick(message):
 #     if message.text.lower() == "исходный код бота":
 #         webbrowser.open("https://github.com/stormcage139/test/blob/master/test1.py")
@@ -118,14 +149,22 @@ def users_list(call):
 #     bot.register_next_step_handler(message, onclick)
 
 
-@bot.message_handler(commands=["hello"])
-def start(message):
-    bot.send_message(message.chat.id, f"Привет, {message.from_user.last_name} {message.from_user.first_name}")
+# @bot.message_handler(commands=["hello"])
+# def start(message):
+#     if authorized:
+#         bot.send_message(message.chat.id, f"Привет, {message.from_user.last_name} {message.from_user.first_name}")
+#     else:
+#         markup = types.ReplyKeyboardMarkup()
+#         btn1 = types.KeyboardButton("/Авторизация")
+#         btn2 = types.KeyboardButton("/Регистрация")
+#         markup.row(btn1, btn2)
+#         bot.send_message(message.chat.id, f"Авторизируйтесь путем написания /Авторизация или нажмите на кнопку",
+#                          reply_markup=markup)
 
 
-@bot.message_handler(commands=["user"])
-def qqq(message):
-    bot.send_message(message.chat.id, message)
+# @bot.message_handler(commands=["user"])
+# def qqq(message):
+#     bot.send_message(message.chat.id, message)
 
 
 # @bot.message_handler(commands=["auto"])
